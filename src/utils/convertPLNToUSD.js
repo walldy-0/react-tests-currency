87642bee9c59bcd874a227a7a8b9737a900bb959
Waldemar Szymon <walldy@tlen.pl>
2023-04-11 13:36:13 +0200

    [kodilla task 21.2] Add tests for 'convertPLNToUSD' function

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,5 +1,17 @@
 export const convertPLNToUSD = (PLN) => {
 
+  if (PLN === undefined || (typeof PLN === 'string' && Object.prototype.toString.call(PLN) === '[object String]')) {
+    return NaN;
+  }
+
+  if (typeof PLN !== 'number' && !(typeof PLN === 'string' && Object.prototype.toString.call(PLN) === '[object String]')) {
+    return 'Error';
+  }
+
+  if (PLN < 0) {
+    return '$0.00';
+  }
+
   const PLNtoUSD = PLN / 3.5;
   
   const formatter = new Intl.NumberFormat('en-US', {
